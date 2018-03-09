f96f4b0b2074150f771e9671a384cd84798f1817
Nicholas Ionata <n.ionata129@gmail.com>
2018-03-09 11:11:01 -0500

    Testing rss reader

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RSSParser from 'rss-parser';
+import { Card, CardHeader } from 'material-ui';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.renderEpisode = this.renderEpisode.bind(this);
+
+    this.state = {
+      titles: [],
+    }
+  }
+
+  componentWillMount() {
+    var titles = [];
+
+    let parser = new RSSParser();
+    parser.parseURL('https://thewebplatform.libsyn.com/rss', function(err, feed) {
+      console.log(feed.title);
+      feed.items.forEach(function(entry) {
+        console.log(entry.title)
+        titles.push(entry.title);
+      })
+
+      this.setState({titles})
+    }.bind(this))
+  }
+
+  renderEpisode(item, index) {
+    return (
+      <Card key={index}>
+        <CardHeader title={item} subheader={""} />
+      </Card>
+    )
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +48,7 @@ class App extends Component {
         <p className="App-intro">
           We are still working on the website. Check back soon!
         </p>
+        {this.state.titles.map(this.renderEpisode)}
       </div>
     );
   }
